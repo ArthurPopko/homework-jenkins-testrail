03d61fc8ded615c62aac4cad831a8726b017590f
QAfun <arthur.popko@icloud.com>
2021-10-27 20:51:28 +0300

    redandunt packages are deleted

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,8 +16,8 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
-module.exports = (on, config) => {
-    on('task', {
-        failed: require('cypress-failed-log/src/failed')(),
-    })
-}
+// module.exports = (on, config) => {
+//     on('task', {
+//         failed: require('cypress-failed-log/src/failed')(),
+//     })
+// }
